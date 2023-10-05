dfeb4687ec8c5dfb2c417fb1ff18ca4ba82fab67
subash7280 <subasheswaramoorthi143@gmail.com>
2023-10-05 20:30:02 +0530

    Add abbrevation on HomePage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -15,7 +15,7 @@ const HomePage = () => {
             {/* <p className="introPara">Dedicated and hardworking professional with a strong work ethic <br /> and adaptability, committed to contributing to team success.</p> */}
             {/* <h2 className="introPara">Welcome to my site.</h2> */}
             <h2 className="introPara">Now, Come have a <span className='webSight' >Web-Sight</span></h2>
-            <Link><button className="btn" disabled ><img src={btnImg} alt="" className='btnImg' />Hire me!</button></Link>
+            <abbr title="Not Yet Fired"><Link><button className="btn" disabled ><img src={btnImg} alt="" className='btnImg' />Hire me!</button></Link></abbr>
         </div>
         <img src={background} alt="Profile Image" className="bg" />
     </section>
